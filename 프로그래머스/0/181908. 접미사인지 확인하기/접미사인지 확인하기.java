e6daec1527d1a993fcff9c3421eea73e7d866c92
uskkkk <dntjd4562@gmail.com>
2024-02-01 14:28:46 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 65.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        
+        int answer = my_string.endsWith(is_suffix) ? 1 : 0;
+        return answer;
+    }
+}
