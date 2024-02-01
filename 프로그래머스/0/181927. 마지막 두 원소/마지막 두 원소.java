2a54c8b4f1be5b379bb7c1ea0a9b6d91f6bd2def
uskkkk <dntjd4562@gmail.com>
2024-02-01 09:55:29 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        answer = Arrays.copyOf(num_list,num_list.length + 1);
+        
+        if(num_list[num_list.length-1] > num_list[num_list.length-2]) {
+            answer[answer.length-1] = num_list[num_list.length-1] - num_list[num_list.length-2];
+        } else {
+            answer[answer.length-1] = num_list[num_list.length-1] * 2;
+        }
+        return answer;
+    }
+}
