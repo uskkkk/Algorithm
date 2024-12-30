116a2362f925c78efafa4c9bac75eb19e2697404
uskkkk <dntjd4562@gmail.com>
2024-12-30 17:17:31 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if (a % 2 != 0 && b % 2 != 0) {
+            answer = (a * a) + (b * b);
+        } else if (a % 2 != 0 || b % 2 != 0 ) {
+            answer = (a + b) * 2;
+        } else {
+            answer = a > b ? a - b : b - a;
+        }
+        return answer;
+    }
+}
